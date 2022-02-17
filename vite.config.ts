a8ce076af50ead0cad71048ea2ea416b792a07ac
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2022-02-17 10:34:14 +0100

    unnecessary packages removed

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import inject from '@rollup/plugin-inject'
-import process from "process"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: process.env.NODE_ENV === "production" ? "/punk-frontend/" : "/",
-  build: {
-    rollupOptions: {
-      plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
-    },
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-  },
 })
