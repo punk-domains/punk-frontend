0c0249defc0f7373f1a1f912334199aa1d7f5939
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2022-02-17 10:12:49 +0100

    GH Actions main.yml

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import inject from '@rollup/plugin-inject'
+import process from "process"
 
 // https://vitejs.dev/config/
 export default defineConfig({
