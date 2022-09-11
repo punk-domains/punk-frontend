5933a4b53514766fb04d2df4645488f59120b500
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2022-09-11 22:23:22 +0200

    Revert "Orbis decentralized chat implemented"
    This reverts commit c68001b3aff96a81db21d70378f463077a214543.

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
 import { defineConfig } from 'vite'
-//import vue from '@vitejs/plugin-vue'
-import veauryVitePlugins from 'veaury/vite'
+import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    //vue()
-    veauryVitePlugins({
-      type: 'vue'
-    })
-  ],
-  build: { target: "es2020" },
-  optimizeDeps: {
-    esbuildOptions: { target: "es2020", supported: { bigint: true } },
-  },
-  define: {
-    "global": {},
-  },
+  plugins: [vue()],
 })
